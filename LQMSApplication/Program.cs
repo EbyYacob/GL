9be49fff50757767edea616d3347710b59ce4d54
EbyYacob <ebygodlandit@gmail.com>
2025-07-31 11:07:26 +0530

    commit

--- LQMSApplication/Program.cs
+++ LQMSApplication/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddSingleton<DapperService>();
 builder.Services.AddSingleton<CryptoService>(); // OR AddScoped<CryptoService>();
 
 
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
